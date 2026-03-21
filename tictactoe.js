8077bcec297f7dd1a9ae028b685481514fa53cb3
Meringue-25 <yuki.li3465@gmail.com>
2026-03-20 23:03:02 -0700

    i made the player swaps stuff

---
+++ tictactoe.js
@@ -0,0 +1,26 @@
+let playerO = "O";
+let playerX = "X";
+let curPlayer = playerO;
+
+let gameboard = ["", "", "", "", "", "", "", "", ""]
+let boardCubes;
+
+window.onload = function()  {
+    boardCubes = document.getElementsByClassName("board-cube");
+    for (let cube of boardCubes) {
+        cube.addEventListener("click", placeCube);
+    }
+}
+
+function placeCube() {
+
+    const index = parseInt(this.getAttribute("data-cell-index"));
+    if (gameboard[index] != "") {
+        return;
+    }
+
+    this.innerText = curPlayer;
+    gameboard[index] = curPlayer;
+
+    curPlayer = (curPlayer == playerO) ? playerX : playerO;
+}
