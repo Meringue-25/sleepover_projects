27adaf6548038f285ba37edca7de42c63d6204c5
Meringue-25 <yuki.li3465@gmail.com>
2026-03-21 21:02:50 -0700

    im not done making my winning conditions but i need to call my aunt so ill come back to this in 15 minutes

--- tictactoe.js
+++ tictactoe.js
@@ -5,6 +5,17 @@ let curPlayer = playerO;
 let gameboard = ["", "", "", "", "", "", "", "", ""]
 let boardCubes;
 
+let winningcubes = [
+    [0, 1, 2]
+    [3, 4, 5]
+    [6, 7, 8]
+    [0, 3, 6]
+    [1, 4, 7]
+    [2, 5, 8]
+    [0, 4, 8]
+    [2, 4, 6]
+];
+
 window.onload = function()  {
     boardCubes = document.getElementsByClassName("board-cube");
     for (let cube of boardCubes) {
@@ -24,5 +35,14 @@ function placeCube() {
 
     curPlayer = (curPlayer == playerO) ? playerX : playerO;
 
-    checkwinner()
+function checkwinner() {
+    for (let winningcube of winningcubes) {
+        let a = gameboard[wincube[0]];
+        let b = gameboard[wincube[1]];
+        let c = gameboard[wincube[2]];
+
+        if (a == b && b == c && a != "") {
+            for (let i = 0;)
+        }
+    }
 }
