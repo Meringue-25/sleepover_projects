51b2ae92708e60655d043214866e7f31a9640049
Meringue-25 <yuki.li3465@gmail.com>
2026-03-22 00:36:35 -0700

    AYYY IT WORKS!

--- tictactoe.js
+++ tictactoe.js
@@ -1,18 +1,20 @@
 let playerO = "O";
 let playerX = "X";
+
+// first player is always O
 let curPlayer = playerO;
 
 let gameboard = ["", "", "", "", "", "", "", "", ""]
 let boardCubes;
 
 let winningcubes = [
-    [0, 1, 2]
-    [3, 4, 5]
-    [6, 7, 8]
-    [0, 3, 6]
-    [1, 4, 7]
-    [2, 5, 8]
-    [0, 4, 8]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
     [2, 4, 6]
 ];
 
@@ -26,25 +28,31 @@ window.onload = function()  {
 function placeCube() {
 
     const index = parseInt(this.getAttribute("data-cell-index"));
-    if (gameboard[index] != "") {
+    if (gameboard[index] !== "") {
         return;
     }
 
-    this.innerText = curPlayer;
     gameboard[index] = curPlayer;
-
+    this.innerText = curPlayer;
+   
+    // change player
     curPlayer = (curPlayer == playerO) ? playerX : playerO;
 
+    // check winner
+    checkwinner();
+
+}
+
 function checkwinner() {
-    for (let winningcube of winningcubes) {
+    for (let wincube of winningcubes) {
         let a = gameboard[wincube[0]];
         let b = gameboard[wincube[1]];
         let c = gameboard[wincube[2]];
 
         if (a == b && b == c && a != "") {
-            for (let i = 0; i , gameboard.length; i++) {
-                if (winningcube.includes(i)) {
-                    gameCell
+            for (let i = 0; i < gameboard.length; i++) {
+                if (wincube.includes(i)) {
+                    boardCubes[i].classList.add("winning-board-cube");
                 }
             }
         }
